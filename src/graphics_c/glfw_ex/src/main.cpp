5899bcd1bd85b7b001703501a91bba3feceae68b
Mikula Mikhail <mikula.mikhail@gmail.com>
2023-06-03 23:44:07 +0300

    OpenGL glfw_ex created class for shaders returns segmentation fault core dump

--- src/graphics_c/glfw_ex/src/main.cpp
+++ src/graphics_c/glfw_ex/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include "Renderer/ShaderProgram.h"
 
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
@@ -92,21 +95,14 @@ int main(int argc, char* argv[])
 
   glClearColor(1, 1, 0, 1); // YELLOW
 
-  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vs, 1, &vertex_shader, nullptr);
-  glCompileShader(vs);
-
-  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fs, 1, &fragment_shader, nullptr);
-  glCompileShader(fs);
-
-  GLuint shader_program = glCreateProgram();
-  glAttachShader(shader_program, vs);
-  glAttachShader(shader_program, fs);
-  glLinkProgram(shader_program);
-
-  glDeleteShader(vs);
-  glDeleteShader(fs);
+  std::string vertexShader(vertex_shader);
+  std::string fragmentShader(fragment_shader);
+  Renderer::ShaderProgram shaderProgram(vertex_shader, fragment_shader);
+  if (!shaderProgram.isCompiled())
+  {
+    std::cerr << "Can't create shader program!" << std::endl;
+    return -1;
+  }
 
   GLuint points_vbo = 0;
   glGenBuffers(1, &points_vbo);
@@ -136,7 +132,7 @@ int main(int argc, char* argv[])
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glUseProgram(shader_program);
+    shaderProgram.use();
     glBindVertexArray(vao);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
