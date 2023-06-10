180b7e3c66f6997b4295a1ccb1d8705ce5e3a99b
Mikula Mikhail <mikula.mikhail@gmail.com>
2023-06-10 12:52:13 +0300

    raylib setup ready for porting smoothlife

--- src/game_of_life/main.c
+++ src/game_of_life/main.c
@@ -11,24 +11,33 @@ int main(void)
   InitWindow(screen_width, screen_height, "SmoothLife GPU");
   SetTargetFPS(60);
 
-  Image image = GenImagePerlinNoise(screen_width, screen_height, 0, 0, 2.0f);
+  Image image = GenImagePerlinNoise(screen_width, screen_height, 0, 0, 5.0f);
   // Image image = GenImageCellular(screen_width, screen_height, screen_width/6);
   // Image image = GenImageWhiteNoise(screen_width, screen_height, .5f);
   
-  // Texture2D texture = LoadTextureFromImage(image);
-
-  RenderTexture2D texture = LoadRenderTexture(screen_width, screen_height);
-  UpdateTexture(texture.texture, image.data);
+  RenderTexture2D state[2];
+  state[0] = LoadRenderTexture(screen_width, screen_height);
+  state[1] = LoadRenderTexture(screen_width, screen_height);
+  UpdateTexture(state[0].texture, image.data);
 
   Shader shader = LoadShader(NULL, "./smoothlife.fs");
   
+  size_t i = 0; // swap between 0, 1 by using i = 1 - i;
+
   while (!WindowShouldClose()) {
-    BeginDrawing();
-      ClearBackground(RED);
+    BeginTextureMode(state[1 - i]);
+      ClearBackground(BLACK);
       BeginShaderMode(shader);
-      DrawTexture(texture.texture, 0, 0, BLUE);
+        DrawTexture(state[i].texture, 0, 0, BLUE);
       EndShaderMode();
+    EndTextureMode();
+
+    BeginDrawing();
+      ClearBackground(BLACK);
+      DrawTexture(state[1 - i].texture, 0, 0, BLUE);
     EndDrawing();
+
+    i = 1 - i; // swap between 0, 1
   }
 
   CloseWindow();
