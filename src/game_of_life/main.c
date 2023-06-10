caf79d8f6e8a634c9e0a5a7900ccb2daf816933c
Mikula Mikhail <mikula.mikhail@gmail.com>
2023-06-10 17:47:36 +0300

    added TextureWrap

--- src/game_of_life/main.c
+++ src/game_of_life/main.c
@@ -9,16 +9,20 @@ int main(void)
   int screen_height = 600;
 
   InitWindow(screen_width, screen_height, "SmoothLife GPU");
-  SetTargetFPS(10);
+  SetTargetFPS(5);
 
   Image image = GenImagePerlinNoise(screen_width, screen_height, 0, 0, 10.0f);
   // Image image = GenImageCellular(screen_width, screen_height, screen_width/6);
-  // Image image = GenImageWhiteNoise(screen_width, screen_height, .5f);
+  // Image image = GenImageWhiteNoise(screen_width, screen_height, 0.9f);
   
   RenderTexture2D state[2];
+
   state[0] = LoadRenderTexture(screen_width, screen_height);
-  state[1] = LoadRenderTexture(screen_width, screen_height);
+  SetTextureWrap(state[0].texture, TEXTURE_WRAP_REPEAT);
   UpdateTexture(state[0].texture, image.data);
+
+  state[1] = LoadRenderTexture(screen_width, screen_height);
+  SetTextureWrap(state[1].texture, TEXTURE_WRAP_REPEAT);
 
   Shader shader = LoadShader(NULL, "./smoothlife.fs");
   Vector2 resolution = {screen_width, screen_height};
@@ -28,19 +32,19 @@ int main(void)
   size_t i = 0; // swap between 0, 1 by using i = 1 - i;
 
   while (!WindowShouldClose()) {
-    BeginTextureMode(state[1 - i]);
+    BeginTextureMode(state[1]);
       ClearBackground(BLACK);
       BeginShaderMode(shader);
-        DrawTexture(state[i].texture, 0, 0, WHITE);
+        DrawTexture(state[0].texture, 0, 0, WHITE);
       EndShaderMode();
     EndTextureMode();
 
     BeginDrawing();
       ClearBackground(BLACK);
-      DrawTexture(state[1 - i].texture, 0, 0, WHITE);
+      DrawTexture(state[1].texture, 0, 0, WHITE);
     EndDrawing();
 
-    // i = 1 - i; // swap between 0, 1
+    //i = 1 - i; // swap between 0, 1
   }
 
   CloseWindow();
--- src/game_of_life/smoothlife.fs
+++ src/game_of_life/smoothlife.fs
@@ -1,60 +1,75 @@
 #version 330 
 
-
-
-
-
-
-
-const int ra = 9; // ring outer
-const int ri = ra/3; // ring inner
-const float alpha = 0.028; // alpha_n
-// const float alpha_m = 0.147f;
+const float ra = 9;
+const float ri = ra/3;
+const float alpha = 0.028;
+// const float alpha_m = 0.147;
 const float b1 = 0.278;
 const float b2 = 0.368;
 const float d1 = 0.267;
 const float d2 = 0.445;
 const float dt = 0.05;
 
+float sigma_1(float x, float a);
+float sigma_2(float x, float a, float b);
+float sigma_m(float x, float y, float m);
+float s(float n, float m);
+float grid(float x, float y);
 
-
-
-
-
-
-
-
-
-// Input vertex attributes (from vertex shader)
 in vec2 fragTexCoord;
 in vec4 fragColor;
 
-// Input uniform values
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
-
-// Output fragment color
-out vec4 finalColor;
-
 uniform vec2 resolution;
 
+out vec4 finalColor;
 
-
-
-
-int emod(int a, int b)
+void main()
 {
-  return (a%b + b)%b;
+  float cx = floor(fragTexCoord.x*resolution.x);
+  float cy = floor(fragTexCoord.y*resolution.y);
+  float m = .0, M = .0; 
+  float n = .0, N = .0;
+
+  float sq_ri = ri*ri, sq_ra = ra*ra;
+
+  for (float dy = -(ra - 1.0); dy <= (ra - 1.0); dy += 1.0) {
+    for (float dx = -(ra - 1.0); dx <= (ra - 1.0); dx += 1.0) {
+      
+      float x = cx + dx;
+      float y = cy + dy;
+
+      float sq_dx = dx*dx, sq_dy = dy*dy;
+
+      if (sq_dx + sq_dy <= sq_ri) {
+        m += grid(y, x);
+        M += 1.0;
+      } else if (sq_dx + sq_dy <= sq_ra) {
+        n += grid(y, x);
+        N += 1.0;
+      }
+
+    }
+  }
+
+  n /= N;
+  m /= M;
+  float q = s(n, m);
+  float diff = 2.0*q - 1.0;
+  float v = grid(cx, cy) + dt*diff;
+
+  finalColor = vec4(v, v, v, 1);
 }
 
 float sigma_1(float x, float a)
 {
-  return 1.0/(1.0 + exp(-(x - a)*4/alpha));
+  return 1.0/(1.0 + exp(-(x - a)*4.0/alpha));
 }
 
 float sigma_2(float x, float a, float b)
 {
-  return sigma_1(x, a)*(1 - sigma_1(x, b));
+  return sigma_1(x, a)*(1.0 - sigma_1(x, b));
 }
 
 float sigma_m(float x, float y, float m)
@@ -67,56 +82,10 @@ float s(float n, float m)
   return sigma_2(n, sigma_m(b1, d1, m), sigma_m(b2, d2, m));
 }
 
-float grid(int x, int y)
+float grid(float x, float y)
 {
-  float tx = float(x)/resolution.x;
-  float ty = float(y)/resolution.y;
+  float tx = x/resolution.x;
+  float ty = y/resolution.y;
   vec4 t = texture(texture0, vec2(tx, ty));
   return max(max(t.x, t.y), t.z);
-}
-
-
-
-
-
-
-void main()
-{
-
-  int cx = int(floor(fragTexCoord.x*resolution.x));
-  int cy = int(floor(fragTexCoord.y*resolution.y));
-  float m = 0, M = 0; 
-  float n = 0, N = 0;
-
-  float sq_ri = ri*ri, sq_ra = ra*ra;
-
-  for (int dy = -(ra - 1); dy <= (ra - 1); ++dy) {
-    for (int dx = -(ra - 1); dx <= (ra - 1); ++dx) {
-      
-      int x = emod(cx + dx, int(resolution.x));
-      int y = emod(cy + dy, int(resolution.y));
-
-      float sq_dx = dx*dx, sq_dy = dy*dy;
-
-      if (sq_dx + sq_dy <= sq_ri) {
-        m += grid(y, x);
-        M++;
-      } else if (sq_dx + sq_dy <= sq_ra) {
-        n += grid(y, x);
-        N++;
-      }
-
-    }
-  }
-
-  n /= N;
-  m /= M;
-  float q = s(n, m);
-  float diff = 2*q - 1;
-  float v = grid(cx, cy) + dt*diff;
-
-
-  // finalColor = texture(texture0, fragTexCoord)*fragColor(0, 1, 0, 1);
-  // finalColor = vec4(fragTexCoord.x, fragTexCoord.y, 0, 1);
-  finalColor = vec4(v, v, v, 1);
 }
